fbf7ddcfe8ac353bf36dc79ec327c6343f1f09a0
Dženan Ramić <dzenan.ramic1210@gmail.com>
2026-05-01 23:30:21 +0200

    new exercises

---
+++ ch03_formatting/isbn_identifier.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int gs1_prefix, group_identifier, publisher_code, item_number, check_digit;
+
+    printf("Enter ISBN: ");
+
+    scanf("%d-%d-%d-%d-%d", &gs1_prefix, &group_identifier, &publisher_code, &item_number, &check_digit);
+
+    printf("GS1 prefix: %d\nGroup identifier: %d\nPublisher code: %d\nItem number: %d\nCheck digit: %d\n", gs1_prefix, group_identifier, publisher_code, item_number, check_digit);
+}
